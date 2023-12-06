825cf989164433bf06dc42394514bbdb67771b7f
Pedro Monteiro <pedromont436@gmail.com>
2023-12-05 23:28:55 -0300

    primeiro commit

---
+++ src/test/java/com/pedromonteiro/java21/Java21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromonteiro.java21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
